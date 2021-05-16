fc3eaa5813820ea3176f6f2786292c73c3f18c8f
unixneo <neo@unix.com>
2021-05-16 10:38:22 +0700

    FIX: Typo.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-unix-locales
 # about: Overrides some Discourse phrases
-# version: 0.11
+# version: 0.12
 # date: 16 May 2021
 # authors: Neo
 # url: https://github.com/unixneo/discourse-unix-locales.git
