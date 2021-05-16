83825f4d2c8f767c05136c9d6b9e97e926ed93a7
unixneo <neo@unix.com>
2021-05-16 15:50:15 +0700

    REFACTOR:   Changes by vbe.

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-unix-locales
 # about: Overrides some Discourse phrases
-# version: 0.14
+# version: 0.15
 # date: 16 May 2021
 # authors: Neo
 # url: https://github.com/unixneo/discourse-unix-locales.git
