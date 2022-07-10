61e9d8530385383a406ab651520c633e39dd8bb5
neounix <neo@unix.com>
2022-07-10 19:39:49 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-unix-locales
 # about: Overrides some Discourse phrases
-# version: 0.16
+# version: 0.161
 # date: 10 July 2022
 # authors: Neo
 # url: https://github.com/unixneo/discourse-unix-locales.git
