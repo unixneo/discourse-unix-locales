981a2c96567e920eed308c9d5b0deb663ed6c1ec
neounix <neo@unix.com>
2024-04-01 19:45:02 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,13 @@
 
 # name: discourse-unix-locales
 # about: Overrides Some OOTB Discourse Phrases
-# version: 0.161
-# date: 10 July 2022
+# version: 0.17
+# date: 1 April 2024
 # authors: Neo
 # url: https://github.com/unixneo/discourse-unix-locales.git
 
 PLUGIN_NAME = "discourse-unix-locales"
+
+enabled_site_setting :enable_discourse_unix_locales
 
 
